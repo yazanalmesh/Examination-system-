9ac9c5517b1bc15f66f2ec5f8af1cd9dbc6142d5
sobhisoos <sobhisoos@gmail.com>
2023-10-19 18:31:55 +0300

    account

--- project/settings.py
+++ project/settings.py
@@ -46,6 +46,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'accounts',
+    'exam',
 ]
 
 ROOT_URLCONF = 'project.urls'
