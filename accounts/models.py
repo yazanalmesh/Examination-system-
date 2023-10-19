9da66ce21e6941bb4214aea825d78f0a0d1caacf
sobhisoos <sobhisoos@gmail.com>
2023-10-19 18:28:33 +0300

    account

--- accounts/models.py
+++ accounts/models.py
@@ -3,25 +3,27 @@ from django.db import models
 
 # Create your models here.
 
-
-class Students(models.Model):
-    username = models.CharField()
-    specalize = models.ForeignKey(Specalizes, on_delete=models.CASCADE)
-    group = models.ForeignKey(Categories , on_delete=models.CASCADE)
-
-class Teachers(models.Model):
-    username = models.CharField(unique=True)
-    password = models.CharField()
-
-class Categories(models.Model):
-    cat_number = models.IntegerField(unique=True)
-    specalize = models.ForeignKey(Specalizes, on_delete=models.CASCADE)
-
-    def __unicode__(self):
-        return self.cat_number
-
-class Specalizes(models.Model):
+class Spec(models.Model):
     spec_name = models.CharField(unique=True)
 
     def __unicode__(self):
         return self.spec_name
+
+class Category(models.Model):
+    cat_number = models.IntegerField(unique=True)
+    specalize = models.ForeignKey(Spec, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return self.cat_number
+    
+class Student(models.Model):
+    username = models.CharField()
+    specalize = models.ForeignKey(Spec, on_delete=models.CASCADE)
+    group = models.ForeignKey(Category , on_delete=models.CASCADE)
+
+class Teacher(models.Model):
+    username = models.CharField(unique=True)
+    password = models.CharField()
+
+
+
