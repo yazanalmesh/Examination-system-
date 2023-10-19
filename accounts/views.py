96d29a380fd9ed7b5096ec28022c86dc5fdbadc3
sobhisoos <sobhisoos@gmail.com>
2023-10-19 15:29:39 +0300

    accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import TeacherLoginForm
-
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 
